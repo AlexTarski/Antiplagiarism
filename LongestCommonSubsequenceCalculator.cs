50b8618841062a4f62dfe4a21b26bffcc7f9c63f
Alex Tarski <Alex.F.L.O.W@yandex.ru>
2024-06-20 17:24:32 +0300

    Implepent the LCSCalculator class functionality, upd README

--- LongestCommonSubsequenceCalculator.cs
+++ LongestCommonSubsequenceCalculator.cs
@@ -13,11 +13,47 @@ public static class LongestCommonSubsequenceCalculator
 
 	private static int[,] CreateOptimizationTable(List<string> first, List<string> second)
 	{
-		throw new NotImplementedException();
-	}
+        var opt = new int[first.Count + 1, second.Count + 1];
+        for (var i = 0; i <= first.Count; ++i) opt[i, 0] = 0;
+        for (var i = 0; i <= second.Count; ++i) opt[0, i] = 0;
+        for (var i = 1; i <= first.Count; ++i)
+            for (var j = 1; j <= second.Count; ++j)
+            {
+                if (first[i - 1] == second[j - 1])
+                    opt[i, j] = opt[i - 1, j - 1] + 1;
+                else
+                {
+                    opt[i, j] = Math.Max(opt[i - 1, j], opt[i, j - 1]);
+                }
+            }
 
-	private static List<string> RestoreAnswer(int[,] opt, List<string> first, List<string> second)
-	{
-		throw new NotImplementedException();
-	}
+        return opt;
+    }
+
+    private static List<string> RestoreAnswer(int[,] opt, List<string> first, List<string> second)
+    {
+        List<string> answer = new ();
+        int i = first.Count;
+        int j = second.Count;
+
+        while (i > 0 && j > 0)
+        {
+            if (first[i - 1] == second[j - 1])
+            {
+                answer.Insert(0, first[i - 1]);
+                i--;
+                j--;
+            }
+            else if (opt[i - 1, j] > opt[i, j - 1])
+            {
+                i--;
+            }
+            else
+            {
+                j--;
+            }
+        }
+
+        return answer;
+    }
 }
--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # Antiplagiarism
+
+This project represents the use of DP concepts in detecting plagiarism in documents.
+
+LevenshteinCalculator class takes a list of documents as input and returns
+a list of pair comparisons of all documents between each other.
+To implement this comparison, a modified Levenshtein algorithm was used:
+it analyzes not the character sequence but the sequence of tokens - lexical items.
+
+For example, the line "force = mass * acceleration" consists of 5 tokens: 'force', '=', 'mass', '*', 'acceleration'.
+
+The input document is represented by a token sequence.
+
+If two tokens are different, the cost of replacing is calculated by the GetTokenDistance method of TokenDistanceCalculator class.
+Cost of add/delete equals 1.
+
+Calculate method in the LongestCommonSubsequentCalculator class computes the longest common subsequence (LCS)
+in input documents (represented by a list of tokens).
+
+After launching the project, documents in SuspiciousSources folder would be checked for plagiarism.
+On the console you will see the Levenshtein distance between each pair of documents.
+Also in the root directory of the project would be a html report file.
+In that file you will see common parts of documents founded by algorithms.
